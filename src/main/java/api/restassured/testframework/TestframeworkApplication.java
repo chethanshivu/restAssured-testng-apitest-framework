a9fac47c37b5c5682bc50ca28e6ce70ce00bfdcd
shivaprasadks <chethanshivu555@gmail.com>
2024-10-20 19:59:03 +0530

    Initial setup

---
+++ src/main/java/api/restassured/testframework/TestframeworkApplication.java
@@ -0,0 +1,13 @@
+package api.restassured.testframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestframeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/restassured/testframework/TestframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package api.restassured.testframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
