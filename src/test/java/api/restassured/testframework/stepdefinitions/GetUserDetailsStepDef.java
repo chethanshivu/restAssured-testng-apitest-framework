5119056e64f1cef341d122d2428f82009006b6fb
shivaprasadks <chethanshivu555@gmail.com>
2024-11-21 22:07:53 +0530

    Updated the tests

--- src/test/java/api/restassured/testframework/stepdefinitions/GetUserDetailsStepDef.java
+++ src/test/java/api/restassured/testframework/stepdefinitions/GetUserDetailsStepDef.java
@@ -1,34 +1,38 @@
 package api.restassured.testframework.stepdefinitions;
 
 import api.restassured.testframework.testutils.Constants;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
 public class GetUserDetailsStepDef {
+
+    Response response;
+
     @Given("Endpoint is setup with auth token")
     public void endpointIsSetupWithAuthToken() {
-        when()
-                .get(Constants.GET_USER_API)
-                .then()
-                .statusCode(200)
-                .body("data.id[0]",equalTo(7))
-                .log().all();
+
     }
 
     @When("User hit the endpoint")
     public void userHitTheEndpoint() {
+        response = when().get(Constants.GET_USER_API);
     }
 
-    @Then("Response code should be {string}")
-    public void responseCodeShouldBe(String arg0) {
+    @Then("Response code should be {int}")
+    public void responseCodeShouldBe(int expectedStatusCode) {
+        response.then().statusCode(expectedStatusCode);
     }
 
     @And("Response body should have below data")
-    public void responseBodyShouldHaveBelowData() {
+    public void responseBodyShouldHaveBelowData(DataTable dataTable) {
+        response.then().body("data.id[0]",equalTo(Integer.valueOf(dataTable.asList().get(1))))
+                .log().all();
     }
 }
--- src/test/resources/features/UserDetailsApi.feature
+++ src/test/resources/features/UserDetailsApi.feature
@@ -4,5 +4,7 @@ Feature: Get User Details API Validations
   Scenario: Get user api provides the specific user details
     Given Endpoint is setup with auth token
     When User hit the endpoint
-    Then Response code should be "200 OK"
+    Then Response code should be 200
     And Response body should have below data
+    |UserId|
+    |7     |
